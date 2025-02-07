aa197e40d07de2f7d2c2a161f9e38c71aca17aff
T244 <T244@bijisen.local>
2025-02-07 18:43:21 +0900

    add ngrok

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+NgrokのURL確認
+http://localhost:4040/
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,7 +4,29 @@ class SurveysController < ApplicationController
     end
     def evaluation
         @questions2 = Question.where(q_id: 2)
-        @teacher = ["下橋","山邊","平野"]
+        
+        # 科目ごとに担当教員を振り分ける
+        case @@info[:department]
+        when "医療事務"
+            @teacher = ["下橋","山邊","平野"]
+        when "地域経済医療事務"
+            @teacher = ["下橋","山邊","平野"]
+        when "公務員"
+            @teacher = ["下橋","山邊","平野"]
+        when "動物"
+            @teacher = ["下橋","山邊","平野"]
+        when "IT"
+            @teacher = ["下橋","山邊","永岡", "遠藤"]
+        when "IT(米子)"
+            @teacher = ["井上","長尾"]
+        when "こども総合"
+            @teacher = ["下橋","山邊","平野"]
+        when "国際自動車整備士"
+            @teacher = ["下橋","山邊","平野"]
+        when "国際観光ビジネス"
+            @teacher = ["下橋","山邊","平野"]                            
+        end
+        
     end
     def fin
         @student_number = @@info[:student_number]
