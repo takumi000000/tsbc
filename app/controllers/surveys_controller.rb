56d6fa600d992c155b5d076b56534b0e13f42ab2
T244 <T244@bijisen.local>
2025-02-28 09:02:18 +0900

    fix front-end

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,28 +4,29 @@ class SurveysController < ApplicationController
     end
     def evaluation
         @questions2 = Question.where(q_id: 2)
-        
-        # 科目ごとに担当教員を振り分ける
-        case @@info[:department]
-        when "医療事務"
-            @teacher = ["下橋","山邊","平野"]
-        when "地域経済医療事務"
-            @teacher = ["下橋","山邊","平野"]
-        when "公務員"
-            @teacher = ["下橋","山邊","平野"]
-        when "動物"
-            @teacher = ["下橋","山邊","平野"]
-        when "IT"
-            @teacher = ["下橋","山邊","永岡", "遠藤"]
-        when "IT(米子)"
-            @teacher = ["井上","長尾"]
-        when "こども総合"
-            @teacher = ["下橋","山邊","平野"]
-        when "国際自動車整備士"
-            @teacher = ["下橋","山邊","平野"]
-        when "国際観光ビジネス"
-            @teacher = ["下橋","山邊","平野"]                            
-        end
+        @teacher = @@info[:teacher]
+
+        # 学科ごとに担当教員を振り分ける
+        # case @@info[:department]
+        # when "医療事務"
+        #     @teacher = ["黒田","中井","平野", "遠藤"]
+        # when "地域経済医療事務"
+        #     @teacher = ["黒田","中井","平野", "遠藤"]
+        # when "公務員"
+        #     @teacher = ["石飛","高木","平野", "遠藤"]
+        # when "動物"
+        #     @teacher = ["伊藤","吉岡","三宅", "遠藤"]
+        # when "IT"
+        #     @teacher = ["下橋","山邊","永岡", "遠藤"]
+        # when "IT(米子)"
+        #     @teacher = ["井上","長尾"]
+        # when "こども総合"
+        #     @teacher = ["遠藤","福士","平野"]
+        # when "国際自動車整備士"
+        #     @teacher = ["難波","内田","安食"]
+        # when "国際観光ビジネス"
+        #     @teacher = ["西村"]                            
+        # end
         
     end
     def fin
@@ -33,11 +34,15 @@ class SurveysController < ApplicationController
     end
     def create
         ary = []
+        ary << "School Check"
+        
         Question.where(q_id: 1).size.times do |i|
             ary << params["q#{i+1}".to_sym]
         end
+
         ary << params[:good_points]
         ary << params[:improvement_points]
+
         Answer.create(
             student_number: @@info[:student_number], 
             department: @@info[:department], 
@@ -49,19 +54,29 @@ class SurveysController < ApplicationController
     def create2
         ary = []
         ary << params[:teacher]
+
+        # 選択した担当教員を削除
+        @@info[:teacher].delete(params[:teacher])
+
         Question.where(q_id: 1).size.times do |i|
             ary << params["q#{i+1}".to_sym]
         end
+
         ary << params[:good_points]
         ary << params[:improvement_points]
-        puts "##############create2###############"
-        puts ary
+
         Answer.create(
             student_number: @@info[:student_number], 
             department: @@info[:department], 
             course: @@info[:course], 
             grade: @@info[:grade], 
             content: ary)
-        redirect_to controller: 'surveys', action: 'evaluation'
+            puts Answer.last.content
+
+        if @@info[:teacher].empty?
+            redirect_to controller: 'surveys', action: 'fin'
+        else
+            redirect_to controller: 'surveys', action: 'evaluation'
+        end
     end
 end
--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -15,6 +15,28 @@ class TopsController < ApplicationController
         @@info[:course] = params[:course]
         @@info[:grade] = params[:grade]
         @@info[:content] =  params[:content]
+        # 学科ごとに担当教員を振り分ける
+        case params[:department]
+        when "医療事務"
+            @@info[:teacher] = ["黒田","中井", "大森", "平野", "遠藤(学支)","キャリア演習"]
+        when "地域経済医療事務"
+            @@info[:teacher] = ["中井","黒田", "大森","平野", "遠藤(学支)","キャリア演習"]
+        when "公務員"
+            @@info[:teacher] = ["石飛","高木","遠藤(学支)","キャリア演習"]
+        when "動物"
+            @@info[:teacher] = ["伊藤","𠮷岡","三宅", "平野", "遠藤(学支)","キャリア演習"]
+        when "IT"
+            @@info[:teacher] = ["下橋","山邊","永岡", "遠藤(学支)","キャリア演習"]
+        when "IT(米子)"
+            @@info[:teacher] = ["井上","長尾","キャリア演習"]
+        when "こども総合"
+            @@info[:teacher] = ["遠藤","西田","福士","髙森",
+                                "黒田", "平野", "遠藤(学支)", "オルグン","キャリア演習"]
+        when "国際自動車整備士"
+            @@info[:teacher] = ["難波","内田","安食", "黒田","キャリア演習"]
+        when "国際観光ビジネス"
+            @@info[:teacher] = ["西村優子(にしむらゆうこ)","西村璃子(にしむらりこ)","平野","キャリア演習"]                            
+        end
         redirect_to controller: 'surveys', action: 'index'
     end
 end
